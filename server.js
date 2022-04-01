612e7e6b840a7c521bf5b73ab70b3d6ee1e0ab27
Setiadi <setiadizainal12@gmail.com>
2022-04-01 07:52:09 +0700

    Reouting request server
    Hapus routing sebelumnya yang hanya method dan data
    Menambahkan routing baru dengan method dan url /, /about

--- server.js
+++ server.js
@@ -4,36 +4,39 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
+    const { method, url } = request;
 
-    if(method === 'GET'){
-        response.end('<h1>Halo GET</h1>');
-    }
-
-    if(method === 'POST'){
-        let body = [];
-
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        })
-    }
-
-    if(method === 'PUT'){
-        response.end('<h1>Halo PUT</h1>');
-    }
-    
-    if(method === 'DELETE'){
-        response.end('<h1>Halo DELETE</h1>');
-    }
-    
-};
+    if(url=== '/'){
+        if(method === 'GET'){
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+        
+    } else if(url === '/about'){
+        if(method === 'GET') {
+            response.end(`<h1> Halo ini adalah halaman about</h1>`);
+        } else if(method === 'POST') {
+            let body = [];
  
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+ 
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+        }
+
+    } else {
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
+    
+}; 
  
 const server = http.createServer(requestListener);
  
